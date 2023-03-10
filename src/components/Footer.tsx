937af65a2a51ffe35eea37d2e659fd3979a08bdd
Great Adams <kingifean@gmail.com>
2023-03-10 17:45:38 +0100

    Redesigned footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,68 +3,45 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-    <footer className='flex pt-[20px] h-[40vh] bg-black text-[#fff] font-dm text-[18px] tw-semibold pb-10'>
-     
-        <ul className='flex flex-row pl[75px] text-xs'>
-            <li className='pt-2 pl-[50px] pr-[250px]'>
-                <Link href="/"><img src="/img/logo.png" alt="Qlip logo" /></Link>
-            </li>
+    <footer className='lg:flex bg-black px-20 py-10 text-white'>
+      <div className='py-10'>
+        <img src="/img/logo.png" alt="Qlip logo" />
+      </div>
+      <div className='flex lg:ml-60'>
+        <ul>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>Mint Store</Link>
+          </li>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>Marketplace</Link>
+          </li>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>Roadmap</Link>
+          </li>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>QLIP Token</Link>
+          </li>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>Whitepaper</Link>
+          </li>
         </ul>
-        
-
-        <div className = 'flex flex-row space-x-[30px] pl[75px] font[3rem] text-bolder'>
-        <ul className='pl-[35px] pr-[35px] a:font-sans text-[20px] tw-bolder'>
-            <li className='pt-2'>
-                <Link href="/">Mint Store</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="/about">Marketplace</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="/blog/hello-world">Roadmap</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="/blog/hello-world">Qlip Token</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="/blog/hello-world">Whitepaper</Link>
-            </li>
+        <ul className='pl-10 lg:pl-56'>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>Twitter</Link>
+          </li>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>Telegram</Link>
+          </li>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>Youtube</Link>
+          </li>
+          <li className='pt-4 text-[18px]'>
+            <Link href='/'>Medium</Link>
+          </li>
         </ul>
+      </div>
 
-        <ul className='pl-[35px] pr-[35px] a:font-sans text-[20px] tw-bolder'> 
-            {/* <li className='pt-2'>
-                <Link href="https://www.cryptosmartnow.io/">Website</Link>
-            </li>  */}
-            <li className='pt-2'>
-                <Link href="https://twitter.com/cryptosmartnow">Twitter</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="https://t.me/cryptosmartcommunity">Telegram</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="https://www.youtube.com/results?search_query=cryptosmartnow">Youtube</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="https://www.linkedin.com/company/cryptosmartnow">Medium</Link>
-            </li>
-        </ul>
-        </div>
-        </footer>
-
-         /* <ul className=' footer-child'>
-            <li className='pt-2'>
-                <Link href="https://www.instagram.com/cryptosmart">Instagram</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="https://www.linkedin.com/company/cryptosmartnow">LinkedIn</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="https://www.reddit.com/r/cryptosmartnow">Reddit</Link>
-            </li>
-            <li className='pt-2'>
-                <Link href="https://discord.com/invite/XkWWY6rBcH">Discord</Link>
-            </li>
-        </ul> */
+    </footer>
      
   )
 }
